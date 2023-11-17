f39bcdaa1fa2660781c3935248c7ef3733523d40
ymyoon(OMEN) <ozone753@naver.com>
2023-11-17 23:25:08 +0900

    기능 구현, 코드 정리

--- zombie.py
+++ zombie.py
@@ -142,7 +142,7 @@ class Zombie:
 
     def flee_from_boy(self, r):
         self.state = 'Walk'
-        self.move_slightly_to(self.x - play_mode.boy.x, self.y - play_mode.boy.y)
+        self.move_slightly_to(self.x - play_mode.boy.x, self.y - play_mode.boy.y)  # 왼쪽 아래로 도망
         if self.is_boy_nearby(r):
             return BehaviorTree.RUNNING
         else:
@@ -166,14 +166,14 @@ class Zombie:
         # 시퀀스
         SEQ_wander = Sequence('Wander', a3, a2)  # 랜덤 위치를 정해서 이동
         SEQ_chase_boy = Sequence('소년을 추적', c1, a4)
-        SEL_chase_or_wander = Selector('추적 또는 배회', SEQ_chase_boy, SEQ_wander)
-        SEQ_flee_from_boy = Sequence('소년에서 도망', c3, a6)
+        SEQ_flee_from_boy = Sequence('소년으로부터 도망', c3, a6)
         SEL_flee_or_chase = Selector('도망 또는 추적', SEQ_flee_from_boy, a4)
-        
+        SEQ_is_boy_near = Sequence('소년이 근처에 있으면 행동', c1, SEL_flee_or_chase)
 
+        # SEL_chase_or_wander = Selector('추적 또는 배회', SEQ_chase_boy, SEQ_wander)
         # SEQ_move_to_target_location = Sequence('Move to target location', a1, a2) # 정해진 위치로 이동
         # SEQ_patrol = Sequence('순찰', a5, a2)
 
-        root = Selector('루트', SEL_chase_or_wander, SEQ_flee_from_boy, SEQ_wander)
+        root = Selector('루트', SEQ_is_boy_near, SEQ_wander)
 
         self.bt = BehaviorTree(root)
