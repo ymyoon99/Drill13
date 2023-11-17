4f2af190fef6e8c5844fc52e3e9aa1310e3df223
ymyoon(OMEN) <ozone753@naver.com>
2023-11-17 22:23:09 +0900

    특정위치 순찰 시퀀스 추가

--- zombie.py
+++ zombie.py
@@ -48,6 +48,10 @@ class Zombie:
         self.tx, self.ty = 1000, 1000
         self.build_behavior_tree()
 
+        self.patrol_locations = [(43, 274), (1118, 274), (1050, 494), (575, 804), (235, 991), (575, 804), (1050, 494),
+                                 (1118, 274)]
+        self.loc_no = 0
+
 
     def get_bb(self):
         return self.x - 50, self.y - 50, self.x + 50, self.y + 50
@@ -119,7 +123,9 @@ class Zombie:
             return BehaviorTree.RUNNING
 
     def get_patrol_location(self):
-        pass
+        self.tx, self.ty = self.patrol_locations[self.loc_no]
+        self.loc_no = (self.loc_no + 1) % len(self.patrol_locations)
+        return BehaviorTree.SUCCESS
 
     def build_behavior_tree(self):
         a1 = Action('Set target location', self.set_target_location, 500, 50)
@@ -136,5 +142,8 @@ class Zombie:
         SEQ_chase_boy = Sequence('소년을 추적', c1, a4)
 
         root = SEL_chase_or_flee = Selector('추적 또는 배회', SEQ_chase_boy, SEQ_wander)
+
+        a5 = Action('순찰 위치 가져오기', self.get_patrol_location)
+        SEQ_patrol = Sequence('순찰', a5, a2)
 
         self.bt = BehaviorTree(root)
